a59d57455879cfbcecfa59f9333a25f5d3a9009f
Timm Bäder <mail@baedert.org>
2016-09-20 10:59:59 +0200

    add simple vector implementation

---
+++ src/vec.hpp
@@ -0,0 +1,28 @@
+#ifndef __VEC_HPP
+#define __VEC_HPP
+
+
+class vec2 {
+public:
+  float x = 0;
+  float y = 0;
+
+  vec2& operator += (const vec2& other) {
+    this->x += other.x;
+    this->y += other.y;
+    return *this;
+  }
+
+  vec2& operator * (const float other) {
+    this->x *= other;
+    this->y *= other;
+    return *this;
+  }
+
+  friend vec2 operator + (vec2 lhs, const vec2& rhs) {
+    lhs += rhs;
+    return lhs;
+  }
+};
+
+#endif
