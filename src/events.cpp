f55ef0f3d4c77b096ab60d3a3414b835f6c949d4
Timm Bäder <mail@baedert.org>
2016-09-21 09:31:59 +0200

    events: Remove console output

--- src/events.cpp
+++ src/events.cpp
@@ -5,11 +5,10 @@
 
 bool handleEvents(SDL_Event event, SDL_Window* window) {
   while (SDL_PollEvent(&event) != 0) {
-    printf(" event type progress: %d \n", event.type);
     if (event.type == SDL_QUIT) {
       return false; // running = false
     }
   }
   return true;
-  
+
 }
