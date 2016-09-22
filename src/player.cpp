bcfeb31806a727a9ea4ffb141803214cd02fe209
Timm Bäder <mail@baedert.org>
2016-09-22 18:35:46 +0200

    player: Convert movement to forces

--- src/player.cpp
+++ src/player.cpp
@@ -15,13 +15,11 @@ Player::Player(Level *level) {
 void Player::input(Input *input) {
   if (input->down) {
     if (input->keycode == SDLK_RIGHT) {
-      this->position.x += 5;
+      apply_force(vec2(0.1, 0));
     } else if (input->keycode == SDLK_LEFT) {
-      this->position.x -= 5;
-    } else if (input->keycode == SDLK_UP) {
-      this->position.y -= 5;
-    } else if (input->keycode == SDLK_DOWN) {
-      this->position.y += 5;
+      apply_force(vec2(-0.1, 0));
+    } else if (input->keycode == SDLK_SPACE) {
+      apply_force(vec2(0, -0.1));
     }
   }
 }
@@ -35,13 +33,19 @@ void Player::update(unsigned int delta) {
 
   int center_x = (position.x + width/2) / 32;
   // bottom
+  bool touches_ground = false;
   int bottom_y = (position.y + height) / 32;
   std::cout << bottom_y << std::endl;
   if (level->is_tile_solid(center_x, bottom_y)) {
       // move up, reset y force
       this->current_force.y = 0;
       this->position.y = (bottom_y - 1) * 32;
+      touches_ground = true;
   }
+
+
+  if (touches_ground)
+    apply_force(vec2(- current_force.x / 100, 0)); // Friction!
 }
 
 void Player::render(SDL_Renderer *renderer) {
